6bd4724b56036ae7bd8cd87373611b7b209775a2
Carlos Gomez <isc.carlos.gomez@gmail.com>
2020-03-28 17:11:38 -0400

    Improve arrow position in regards to the link line

--- src/net/sourceforge/plantuml/svek/communication/FocalPointCalculator.java
+++ src/net/sourceforge/plantuml/svek/communication/FocalPointCalculator.java
@@ -1,7 +1,9 @@
 package net.sourceforge.plantuml.svek.communication;
 
 import java.awt.geom.CubicCurve2D;
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import net.sourceforge.plantuml.svek.Line;
 
@@ -48,7 +50,7 @@ class FocalPointCalculator {
         .stream()
         .filter(bezier -> bezier.x2 >= x)
         .findFirst()
-        .map(bezier -> findBezierPointFromX(bezier, x));
+        .map(bezier -> findBezierPoint(bezier, comparingX(x)));
   }
 
   private Optional<Point> findPointFromY(final double y) {
@@ -56,41 +58,15 @@ class FocalPointCalculator {
         .stream()
         .filter(bezier -> bezier.y2 >= y)
         .findFirst()
-        .map(bezier -> findBezierPointFromY(bezier, y));
+        .map(bezier -> findBezierPoint(bezier, comparingY(y)));
   }
 
-  private Point findBezierPointFromX(final CubicCurve2D.Double bezier, final double x) {
-    final double threshold = 5;
-    double t = 0.5;
-    Point point = bezierPoint(bezier, t);
-    int limit = 20;
-    while (Math.abs(point.x - x) > threshold && limit > 0) {
-      if (point.x > x) {
-        t = t - t / 2;
-      } else {
-        t = t + t / 2;
-      }
-      point = bezierPoint(bezier, t);
-      limit--;
-    }
-    return point;
-  }
-
-  private Point findBezierPointFromY(final CubicCurve2D.Double bezier, final double y) {
-    final double threshold = 5;
-    double t = 0.5;
-    Point point = bezierPoint(bezier, t);
-    int limit = 0;
-    while (Math.abs(point.y - y) > threshold && limit > 0) {
-      if (point.y > y) {
-        t = t - t / 2;
-      } else {
-        t = t + t / 2;
-      }
-      point = bezierPoint(bezier, t);
-      limit--;
-    }
-    return point;
+  private Point findBezierPoint(final CubicCurve2D.Double bezier, final Comparator<Point> comparator) {
+    return IntStream.rangeClosed(0, 10)
+        .mapToDouble(i -> i / 10.0)
+        .mapToObj(t -> bezierPoint(bezier, t))
+        .min(comparator)
+        .orElseThrow(() -> new IllegalStateException("Expected Bezier point not found"));
   }
 
   private Point bezierPoint(final CubicCurve2D.Double bezier, final double t) {
@@ -102,6 +78,14 @@ class FocalPointCalculator {
   private double bezierEquation(final double a0, final double a1, final double a2, final double a3, final double t) {
     return Math.pow(1 - t, 3) * a0 + 3 * Math.pow(1 - t, 2) * t * a1 + 3 * (1 - t) * Math.pow(t, 2) * a2
         + Math.pow(t, 3) * a3;
+  }
+
+  private static Comparator<Point> comparingX(final double x) {
+    return Comparator.comparing(point -> Math.abs(x - point.getX()));
+  }
+
+  private static Comparator<Point> comparingY(final double y) {
+    return Comparator.comparing(point -> Math.abs(y - point.getY()));
   }
 
 }
