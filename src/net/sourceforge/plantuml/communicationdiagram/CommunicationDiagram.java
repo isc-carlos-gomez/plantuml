1232f4e118f6203345c4635185b7f6badf82e3ec
Carlos Gomez <isc.carlos.gomez@gmail.com>
2020-09-12 15:14:31 -0400

    Introduce Communication Links
    These links replace regular class links in Communication Diagrams to
    form groups and tune properties like end decorations which will help to
    render lines that resemble Communication Links.

--- src/net/sourceforge/plantuml/communicationdiagram/CommunicationDiagram.java
+++ src/net/sourceforge/plantuml/communicationdiagram/CommunicationDiagram.java
@@ -1,7 +1,11 @@
 package net.sourceforge.plantuml.communicationdiagram;
 
+import java.util.Optional;
+
 import net.sourceforge.plantuml.ISkinSimple;
 import net.sourceforge.plantuml.classdiagram.ClassDiagram;
+import net.sourceforge.plantuml.communicationdiagram.link.CommunicationLink;
+import net.sourceforge.plantuml.cucadiagram.Link;
 
 /**
  * A <a href="https://www.uml-diagrams.org/communication-diagrams.html">UML Communication
@@ -12,7 +16,7 @@ import net.sourceforge.plantuml.classdiagram.ClassDiagram;
  *
  * @author Carlos Gomez
  */
-class CommunicationDiagram extends ClassDiagram {
+public class CommunicationDiagram extends ClassDiagram {
 
   /**
    * Creates a new instance.
@@ -22,6 +26,58 @@ class CommunicationDiagram extends ClassDiagram {
    */
   CommunicationDiagram(final ISkinSimple skinParam) {
     super(skinParam);
+  }
+
+  /**
+   * Removes the last link from this diagram.
+   *
+   * @return the removed link
+   */
+  public Link removeLastLink() {
+    final Link lastLink = getLastLink();
+    removeLink(lastLink);
+    return lastLink;
+  }
+
+  /**
+   * Replaces two links in this diagram.
+   *
+   * @param existingLink
+   *        the link that will be replaced
+   * @param newLink
+   *        the replacing link
+   */
+  public void replaceLink(final Link existingLink, final Link newLink) {
+    removeLink(existingLink);
+    addLink(newLink);
+  }
+
+  /**
+   * Finds the first link that has the same ends (aka. entities) as the given link in this diagram.
+   *
+   * @param link
+   *        the link to compare
+   * @return the found link, if any
+   */
+  public Optional<CommunicationLink> findLinkWithSameEnds(final Link link) {
+    return getLinks().stream()
+        .map(CommunicationLink.class::cast)
+        .filter(communicationLink -> communicationLink.hasSameEnds(link))
+        .findFirst();
+  }
+
+  /**
+   * Finds the first link that has ends (aka. entities) opposite to the given link in this diagram.
+   *
+   * @param link
+   *        the link to compare
+   * @return the found link, if any
+   */
+  public Optional<CommunicationLink> findLinkWithOppositeEnds(final Link link) {
+    return getLinks().stream()
+        .map(CommunicationLink.class::cast)
+        .filter(communicationLink -> communicationLink.hasOppositeEnds(link))
+        .findFirst();
   }
 
 }
---
+++ src/net/sourceforge/plantuml/communicationdiagram/link/CustomLinkLabelRegexResult.java
@@ -0,0 +1,61 @@
+package net.sourceforge.plantuml.communicationdiagram.link;
+
+import java.util.HashMap;
+
+import net.sourceforge.plantuml.command.regex.RegexPartialMatch;
+import net.sourceforge.plantuml.command.regex.RegexResult;
+
+/**
+ * {@link RegexResult} decorator to overwrite link labels.
+ *
+ * @author Carlos Gomez
+ */
+class CustomLinkLabelRegexResult extends RegexResult {
+
+  private static final String LINK_LABEL_KEY = "LABEL_LINK";
+  private final RegexResult delegate;
+  private final String customLinkLabel;
+
+  /**
+   * Creates a new instance.
+   *
+   * @param originalResult
+   *        the original result to decorate with a new link label
+   * @param customLinkLabel
+   *        the new link label
+   */
+  CustomLinkLabelRegexResult(final RegexResult originalResult, final String customLinkLabel) {
+    super(new HashMap<>());
+    this.delegate = originalResult;
+    this.customLinkLabel = customLinkLabel;
+  }
+
+  @Override
+  public String get(final String key, final int num) {
+    if (LINK_LABEL_KEY.equals(key)) {
+      return this.customLinkLabel;
+    }
+    return this.delegate.get(key, num);
+  }
+
+  @Override
+  public RegexPartialMatch get(final String key) {
+    return this.delegate.get(key);
+  }
+
+  @Override
+  public String getLazzy(final String key, final int num) {
+    return this.delegate.getLazzy(key, num);
+  }
+
+  @Override
+  public int size() {
+    return this.delegate.size();
+  }
+
+  @Override
+  public String toString() {
+    return this.delegate.toString();
+  }
+
+}
