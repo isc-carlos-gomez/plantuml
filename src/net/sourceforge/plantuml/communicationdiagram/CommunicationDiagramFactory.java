6b0615223a6a1b5c54d9871e00a8bad138952fa3
Carlos Gomez <isc.carlos.gomez@gmail.com>
2020-09-12 12:48:30 -0400

    Introduce the Communication Diagram
    This diagram is an extension of the Classes Diagram and for now all the
    communication diagrams are rendered as classes diagrams.

---
+++ src/net/sourceforge/plantuml/communicationdiagram/CommunicationDiagramFactory.java
@@ -0,0 +1,45 @@
+package net.sourceforge.plantuml.communicationdiagram;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.plantuml.ISkinSimple;
+import net.sourceforge.plantuml.classdiagram.ClassDiagram;
+import net.sourceforge.plantuml.classdiagram.ClassDiagramFactory;
+import net.sourceforge.plantuml.command.Command;
+
+/**
+ * Factory of {@link CommunicationDiagram}s.
+ *
+ * @author Carlos Gomez
+ */
+public class CommunicationDiagramFactory extends ClassDiagramFactory {
+
+  private final ISkinSimple skinParam;
+
+  /**
+   * Creates a new instance.
+   *
+   * @param skinParam
+   *        skin (look & feel) parameters that will be used to create Communication Diagrams
+   */
+  public CommunicationDiagramFactory(final ISkinSimple skinParam) {
+    super(skinParam);
+    this.skinParam = skinParam;
+  }
+
+  @Override
+  public ClassDiagram createEmptyDiagram() {
+    return new CommunicationDiagram(this.skinParam);
+  }
+
+  @Override
+  @SuppressWarnings("rawtypes")
+  protected List<Command> createCommands() {
+    final List<Command> commands = new ArrayList<>();
+    commands.add(new CommandCommunicationDiagram());
+    commands.addAll(super.createCommands());
+    return commands;
+  }
+
+}
